89a19bc0543db3e92ad3d87b34fd3c277a0fcf3b
gacmen45 <marekgacek45@gmail.com>
2022-11-16 17:25:41 +0100

    export data

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,32 +1,9 @@
 import ProductItem from "./ProductItem"
 import classes from './Products.module.css'
+import PRODUCT_LIST from '../../data/PRODUCT_LIST'
 
-const PRODUCT_LIST = [
-	{
-		id: 'gow',
-		name: 'God of War',
-		category: 'games',
-		price: 79.99,
-	},
-	{
-		id: 'ps5',
-		name: 'PlayStation 5',
-		category: 'consoles',
-		price: 2599,
-	},
-	{
-		id: 'got',
-		name: 'Ghost of Tsushima',
-		category: 'games',
-		price: 139.99,
-	},
-	{
-		id: 'ps4',
-		name: 'PlayStation 4',
-		category: 'consoles',
-		price: 1299,
-	},
-]
+
+
 
 
 
---
+++ src/data/PRODUCT_LIST.js
@@ -0,0 +1,29 @@
+const PRODUCT_LIST = [
+	{
+		id: 'gow',
+		name: 'God of War',
+		category: 'games',
+		price: 79.99,
+	},
+	{
+		id: 'ps5',
+		name: 'PlayStation 5',
+		category: 'consoles',
+		price: 2599,
+	},
+	{
+		id: 'got',
+		name: 'Ghost of Tsushima',
+		category: 'games',
+		price: 139.99,
+	},
+	{
+		id: 'ps4',
+		name: 'PlayStation 4',
+		category: 'consoles',
+		price: 1299,
+	},
+	
+]
+
+export default PRODUCT_LIST
