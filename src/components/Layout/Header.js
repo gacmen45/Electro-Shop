688c4e1d4e46b537ce78f127271b6c0e1a103934
gacmen45 <marekgacek45@gmail.com>
2022-11-17 09:21:50 +0100

    install faawesome react

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -3,6 +3,10 @@ import classes from './Header.module.css'
 import HeaderCartButton from "./HeaderCartButton"
 import { useNavigate } from "react-router-dom"
 
+import ReactDOM from 'react-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faRobot } from '@fortawesome/free-solid-svg-icons'
+
 const Header = props => {
 	let navigate= useNavigate()
 	const routeChange = () => {
@@ -10,10 +14,16 @@ const Header = props => {
 		navigate(path)
 	}
 
+	const element = <FontAwesomeIcon icon={faRobot} />
+
 	return (
 		<Fragment>
 			<header className={classes.header}>
-				<h1 onClick={routeChange}>Electro-Shop</h1>
+				<div onClick={routeChange}>
+				<h1 >Electro-Shop</h1>
+				{element}
+
+				</div>
 				<HeaderCartButton showCartHandler={props.showCartHandler}/>
 			</header>
 		</Fragment>
