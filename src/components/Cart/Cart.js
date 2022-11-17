6b1b121d75c8cff9b37204999e72e86aff2e89d7
gacmen45 <marekgacek45@gmail.com>
2022-11-17 08:31:02 +0100

    add functions to context

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,21 +1,46 @@
+import { useContext } from 'react'
 import classes from './Cart.module.css'
 import Modal from '../UI/Modal'
+import CartContext from '../../store/cart-context'
+import CartItem from './CartItem'
 
-const Cart =(props) => {
-const cartItems = <ul>{[ {id:'gow',name:'God of War', amount:1, price:79.99}].map((item) => <li>{item.name}</li>)}</ul>
+const Cart = props => {
+	const cartCtx = useContext(CartContext)
 
-    return <Modal hideCartHandler={props.hideCartHandler}>
-        {cartItems}
-        <div>
-            <span>Total Amount</span>
-            <span>99.99</span>
-        </div>
-        <div>
-            <button onClick={props.hideCartHandler}>Close</button>
-            <button>Order</button>
-        </div>
-    </Modal>
+const totalAmount = `${cartCtx.totalAmount.toFixed(2)}zł`
+const hasItems= cartCtx.items.length >0
+
+const cartItemAddHandler = item =>{
+    cartCtx.addItem({...item,amount:1})
+
 }
 
+const cartItemRemoveHandler = id => {
+    cartCtx.removeItem(id)
+
+}
+
+	const cartItems = (
+		<ul>
+			{cartCtx.items.map((item) => (
+				<CartItem key={item.id} name={item.name} amount={item.amount} price={item.price} onRemove={cartItemRemoveHandler.bind(null,item.id)} onAdd={cartItemAddHandler.bind(null,item)}/>
+			))}
+		</ul>
+	)
+
+	return (
+		<Modal hideCartHandler={props.hideCartHandler}>
+			{cartItems}
+			<div>
+				<span>Total Amount: </span>
+				<span>{totalAmount}</span>
+			</div>
+			<div>
+				<button onClick={props.hideCartHandler}>Close</button>
+				{hasItems && <button>Order</button>}
+			</div>
+		</Modal>
+	)
+}
 
 export default Cart
